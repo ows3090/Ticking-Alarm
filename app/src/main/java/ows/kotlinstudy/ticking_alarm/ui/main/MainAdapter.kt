ee6974a89edfd958ab7e582469c5aab1e93e07fc
ows3090 <ows3090@naver.com>
2022-10-09 18:21:34 +0900

    style : 어두운 테마에 맞는 리소스 설정
    - MainActivity의 리스트 뷰 아이템 리소스 색상 변경
    - Status Bar 블랙 처리

--- app/src/main/java/ows/kotlinstudy/ticking_alarm/ui/main/MainAdapter.kt
+++ app/src/main/java/ows/kotlinstudy/ticking_alarm/ui/main/MainAdapter.kt
@@ -5,22 +5,3 @@ import androidx.recyclerview.widget.RecyclerView
 import ows.kotlinstudy.ticking_alarm.databinding.ItemViewTimeBinding
 import javax.inject.Inject
 
-class MainAdapter @Inject constructor() : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
-    inner class ViewHolder(
-        private val binding: ItemViewTimeBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-}
